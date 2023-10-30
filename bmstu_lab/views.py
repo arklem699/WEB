8941a74e450acc9db3c77ac27ac86b08fe997f95
arklem <jrsy.tdh@yandex.ru>
2023-10-30 21:39:41 +0300

    Update

--- bmstu_lab/views.py
+++ bmstu_lab/views.py
@@ -59,29 +59,59 @@ def get_list_appointment(request, format=None):
     """
     Возвращает список услуг
     """
-    print('get')
     appointments = Appointment.objects.all()
     serializer = AppointmentSerializer(appointments, many=True)
     return Response(serializer.data)
 
 
-@api_view(['Get'])
-def get_detail_appointment(request, id, format=None):
+@api_view(['Get', 'Post', 'Put', 'Delete'])
+def detail_appointment(request, id, format=None):
     appointment = get_object_or_404(Appointment, id=id)
     if request.method == 'GET':
         """
         Возвращает информацию об услуге
         """
         serializer = AppointmentSerializer(appointment)
-        return Response(serializer.data)
+        return Response(serializer.data)   
     
+    elif request.method == 'POST':    
+        """
+        Добавляет услугу в последнюю заявку
+        """
+        new_appapp, created = AppApp.objects.get_or_create(
+            id_appl = Application.objects.latest('id'),
+            id_appoint = Appointment.objects.get(id=id)
+        )
+        serializer = AppAppSerializer(new_appapp)
+        if created:
+            new_appapp.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        """
+        Обновляет информацию об услуге
+        """
+        serializer = AppointmentSerializer(appointment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':    
+        """
+        Удаляет информацию об услуге
+        """
+        appointment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @api_view(['Post'])
 def post_list_appoinment(request, format=None):    
     """
     Добавляет новую услугу
     """
-    print('post')
     serializer = AppointmentSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -89,57 +119,18 @@ def post_list_appoinment(request, format=None):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['Post'])
-def post_appoinment_in_application(request, id, format=None):    
-    """
-    Добавляет услугу в последнюю заявку
-    """
-    print('post')
-    new_appapp = AppApp.objects.create(
-        id_appl = Application.objects.latest('id'),
-        id_appoint = Appointment.objects.get(id=id)
-    )
-    new_appapp.save()
-    serializer = AppAppSerializer(new_appapp)
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-@api_view(['Put'])
-def put_detail_appointment(request, id, format=None):
-    """
-    Обновляет информацию об услуге
-    """
-    appointment = get_object_or_404(Appointment, id=id)
-    serializer = AppointmentSerializer(appointment, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['Delete'])
-def delete_detail_appointment(request, id, format=None):    
-    """
-    Удаляет информацию об услуге
-    """
-    appointment = get_object_or_404(Appointment, id=id)
-    appointment.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 @api_view(['Get'])
 def get_list_application(request, format=None):
     """
     Возвращает список заявок
     """
-    print('get')
     applications = Application.objects.all()
     serializer = ApplicationSerializer(applications, many=True)
     return Response(serializer.data)
 
 
-@api_view(['Get'])
-def get_detail_application(request, id, format=None):
+@api_view(['Get', 'Delete'])
+def detail_application(request, id, format=None):
     application = get_object_or_404(Application, id=id)
     if request.method == 'GET':
         """
@@ -147,16 +138,12 @@ def get_detail_application(request, id, format=None):
         """
         serializer = ApplicationSerializer(application)
         return Response(serializer.data)
-
-
-@api_view(['Delete'])
-def delete_detail_application(request, id, format=None):    
-    """
-    Удаляет информацию о заявке
-    """
-    application = get_object_or_404(Application, id=id)
-    application.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'DELETE':    
+        """
+        Удаляет информацию о заявке
+        """
+        application.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['Put'])
@@ -186,7 +173,7 @@ def put_status_moderator_application(request, id, format=None):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    return Response({'error': "Нельзя изменить статус на 'Удалён'."}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'error': "Нельзя изменить статус на 'Удалён'."}, status=status.HTTP_403_FORBIDDEN)
 
 
 @api_view(['Delete'])
