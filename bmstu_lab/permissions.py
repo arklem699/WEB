1f84817065950bb64d094e2baef4c658e1e4822a
arklem <jrsy.tdh@yandex.ru>
2024-01-20 14:09:14 +0300

    update

--- bmstu_lab/permissions.py
+++ bmstu_lab/permissions.py
@@ -1,10 +1,32 @@
 from rest_framework import permissions
+from bmstu_lab.models import CustomUser
+from django.conf import settings
+import redis
+
+
+session_storage = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 
 class IsManager(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and (request.user.is_staff or request.user.is_superuser))
+        ssid = request.COOKIES.get("session_id")
+
+        if ssid is not None:
+            user_id = session_storage.get(ssid)
+
+        if user_id is not None:
+            user = CustomUser.objects.get(id=user_id)
+
+        return bool(user and (user.is_staff or user.is_superuser))
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_superuser)
+        ssid = request.COOKIES.get("session_id")
+
+        if ssid is not None:
+            user_id = session_storage.get(ssid)
+
+        if user_id is not None:
+            user = CustomUser.objects.get(id=user_id)
+            
+        return bool(user and user.is_superuser)
