62814c8c2d55907168470befe540c5f88fd5bc22
arklem <jrsy.tdh@yandex.ru>
2024-01-20 14:41:12 +0300

    update

--- bmstu_lab/migrations/views.py
+++ bmstu_lab/migrations/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 
 data = [
-            {'date': '12.10.2023', 'time': '13:00', 'name': 'Ольгина А.С.', 'id': 1},
-            {'date': '13.10.2023', 'time': '14:00', 'name': 'Иванов А.А.', 'id': 2},
-            {'date': '13.10.2023', 'time': '15:00', 'name': 'Мирошкина Д.С.', 'id': 3},
+            {'date': '12.10.2023', 'time': '13:00', 'doctor': 'Ольгина А.С.', 'id': 1},
+            {'date': '13.10.2023', 'time': '14:00', 'doctor': 'Иванов А.А.', 'id': 2},
+            {'date': '13.10.2023', 'time': '15:00', 'doctor': 'Мирошкина Д.С.', 'id': 3},
         ]
 
-def GetOrders(request):
-    return render(request, 'orders.html', {'data' : data })
+def GetAppointments(request):
+    return render(request, 'appointments.html', {'data' : data })
 
-def GetOrder(request, id):
+def GetAppointment(request, id):
     for my_dict in data:
         if my_dict['id'] == id:
-            return render(request, 'order.html', {'data': my_dict})
+            return render(request, 'appointment.html', {'data': my_dict})
 
 def GetQuery(request):
     query = request.GET.get('query', '')
@@ -20,4 +20,4 @@ def GetQuery(request):
     for item in data:
         if query in item["date"]:
             new_data.append(item)
-    return render(request, 'orders.html', {'data': new_data})
+    return render(request, 'appointments.html', {'data': new_data})
