a45141c31ab8caa95a72610182b3d60b7255fcc6
arklem <jrsy.tdh@yandex.ru>
2024-01-31 15:34:03 +0300

    update models.py

--- bmstu_lab/models.py
+++ bmstu_lab/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager, Group, Permission
 
 
 class AppApp(models.Model):
     id_appl = models.ForeignKey('Application', models.CASCADE, db_column='id_appl')
     id_appoint = models.ForeignKey('Appointment', models.CASCADE, db_column='id_appoint')
+    was = models.BooleanField(default=False, verbose_name="Был ли на приёме?")
 
     class Meta:
         managed = True
@@ -18,7 +19,6 @@ class Application(models.Model):
     date_completion = models.DateField(blank=True, null=True)
     moderator = models.CharField(max_length=50, blank=True, null=True)
     status = models.CharField(max_length=50, blank=True, null=True)
-    was = models.BooleanField(default=False, verbose_name="Был ли на приёме?")
 
     class Meta:
         managed = True
@@ -37,6 +37,29 @@ class Appointment(models.Model):
         db_table = 'appointment'
 
 
+class NewUserManager(UserManager):
+    def create_user(self,email,password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email address')
+        
+        email = self.normalize_email(email) 
+        user = self.model(email=email, **extra_fields) 
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(("email адрес"), unique=True)
     username = models.CharField(max_length=30, unique=True, null=True, blank=True, verbose_name="Имя пользователя")
@@ -47,6 +70,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions', blank=True, verbose_name=('user permissions'), help_text=('Specific permissions for this user.'), related_query_name='user', )
 
     USERNAME_FIELD = 'email'
+
+    objects =  NewUserManager()
 
     class Meta:
         managed = True
